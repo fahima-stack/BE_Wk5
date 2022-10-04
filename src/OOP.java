98fe6b0a313c1d23be589b258734cac60dfaa4a1
Fahima Begum <haffahima@gmail.com>
2022-10-04 21:02:48 +0100

    OOP 2nd commit

--- src/OOP.java
+++ src/OOP.java
@@ -2,8 +2,49 @@
 public class OOP {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		/*Until now have been doing procedural programming: writing instructions for a computer to do 1 line at a time. 
+		 * Object Oriented programming (OOP): maintainable code
+		 * 
+		 *DVD Player:
+		 * 	-Height
+		 * 	-Weight
+		 * 	-Width	
+		 * 	-Colour
+		 * 
+		 *Functionality:
+		 * 	-Play
+		 * 	-Fast Forward
+		 *	-Rewind
+		 *	-Pause
+		 *
+		 *DVD
+		 *Properties:
+		 *	-Movie length
+		 *	-Image
+		 *	-Size
+		 *Functionality:
+		 *	-Store
+		 *
+		 *Four pillars of OOP:
+		 *1. Abstraction
+		 *	-Triangle (has 3 angles, lines, 180 degree internal angles). Don't need to know all properties of a triangle to know it is one, that's abstraction.
+		 *  -Table (know what makes a table, if asked what colour is a table, you'd ask what table).
+		 *2. Encapsulation 
+		 *	-Hiding the inner workings of your code so certain methods are only accessible to certain areas of the code. Means the object you create for a class can't be manipulated in a way it's not supposed to. 
+		 *	-E.g. Encapsulation of a car is what the car does on the inside to run, this is private to the user, they don't need to know. User just accesses the public pieces e.g. gas pedal, wheels, brakes etc. 
+		 *3.Inheritance
+		 *	-Just like you inherit traits from parents or ancestors, OOP allows objects or classes to inherit from parent classes.
+		 *4.Polymorphism: 
+		 *	-Animals communicate in different ways e.g. dogs bark, cats meow, humans talk. 
+		 *	-One piece of the class/ one method in a class can be different to take different forms e.g. the communication. 
+		 *	-The "method" communicate can take different forms. 
+		 *
+		 *Class is the tool/ how we structure our code in OOP. 
+		 *Class is like a blueprint e.g. car.
+		 *Object is the actual product developed from the blueprint e.g. volvo audi, toyota. 
+		 *An object is an Instance of a Class. 
+		 */
 	}
 
 }
