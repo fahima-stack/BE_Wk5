5bd4bb3792464d9442acc5e36fffabb6ed481d07
Fahima Begum <haffahima@gmail.com>
2022-10-05 03:03:01 +0100

    Classes + student java final commit

--- src/Classes.java
+++ src/Classes.java
@@ -3,9 +3,22 @@ public class Classes {
 
 	public static void main(String[] args) {
 	
-		StringBuilder name = new StringBuilder();
-		StringBuilder name2 = new StringBuilder();
+		StringBuilder name = new StringBuilder(); //line 6, 7 + 9 are methods because ()
+		StringBuilder name2 = new StringBuilder();  
 		
+		Student student1 = new Student();
+		student1.firstName = "Tom";
+		student1.lastName = "Smith";
+		student1.gradeLevel = 12;
+		student1.phoneNumber = "2480-123-4567"; //prints Hello, my name is Tom Smith/ I am in grade 12/My phone number is 2480-123-4567
+
+		student1.introduce();
+		
+		Student student2 = new Student ("Sammy", "Jones");
+		student2.introduce();
+		
+		Student student3 = new Student("Tom", "Riddle", "904-320-9876", 11);
+		student3.introduce();
 	}
 
 }
@@ -13,5 +26,8 @@ public class Classes {
 /*Classes that come with Java:
  * -E.g. String; StringBuilder, Scanner, Array, ArrayList
  * -Have used and created instances of these classes using 'new' keyword (created an object which is an instance of the class). 
- *      .See line 06/07: you have created Objects called name and name2 that are instances of the Class StringBuilder (data type).    
+ *      .See line 06/07: you have created Objects called name and name2 that are instances of the Class StringBuilder (data type). 
+ * -Using . notation to assign values to our instance of the student class is cumbersome - repeating 5 lines of code for each instance of the student class.
+ * -^ solve using Constructor (line 6 + 7 are Constructors, java gives to classes that don't have.
+ * Constructor is a special method, it's the method you call when you create an instance of it.     
  */
--- src/Student.java
+++ src/Student.java
@@ -1,14 +1,37 @@
 
 public class Student { //Creating Class student. Class names have Pascal casing, not camel casing. All words capitalised. 
 	
+	static int numberOfStudents; //static means each instance of the class will have the common property of numberOfStudents
+	
 	String firstName; 
 	String lastName;
 	String phoneNumber;
 	int gradeLevel;
 	
-	public void introduce( ) { //each instance of the class is going to have first name, last name etc. 
+	public Student() {} //A constructor that has no parameters so can take any of them.
+	
+	public Student(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName; 
+	}
+	
+	public Student(String firstName, String lastName, String phoneNumber, int gradeLevel) { //special method constructor is the only method without a return type. Parameters inside ().
+		this.firstName = firstName; //this. represents the specific instance of the class that's using it at that time. Also points to the variable firstName on line 6, not our parameter on line 12, so assigning parameter value to your instance value.
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+		this.gradeLevel = gradeLevel; 
+		
+	}
+	 
+	
+
+	public void introduce( ) { //public means each instance of the class is going to have first name, last name etc. 
 		System.out.println("Hello, my name is " + firstName + " " + lastName);
 		System.out.println("I am in grade " + gradeLevel);
 		System.out.println("My phone number is " + phoneNumber);
 	}
 }
+
+/*Can overload constructors and have different versions of them.
+ * 
+ */
