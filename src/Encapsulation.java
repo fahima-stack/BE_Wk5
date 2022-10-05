07aa5c2ecd93472ef3637ebfa0cc24e4d071129d
Fahima Begum <haffahima@gmail.com>
2022-10-06 00:36:44 +0100

    Encapsulation first commit

---
+++ src/Encapsulation.java
@@ -0,0 +1,19 @@
+
+public class Encapsulation {
+
+	public static void main(String[] args) {
+		
+		/*Need to encapsulate the object's data and only expose what needs to be used. Done using access modifiers.
+		 * Access modifiers: 
+		 * Public 		accessible everywhere
+		 * Private		only accessible within the class itself
+		 * Protected 	accessible within the class, other classes in the same package, and all subclasses. 
+		 * No Modifier	same as protected, except not accessible in a subclass in different packages. 
+		 * 
+		 * Most common: public and private fields
+		 */
+		
+
+	}
+
+}
--- src/Wk5_ZoomClass.java
+++ src/Wk5_ZoomClass.java
@@ -5,10 +5,10 @@ public class Wk5_ZoomClass {
 		// Fahima Qs
 		//@23 mins Strings are immutable - when passed to a method, they don't change.
 		//Everything else is mutable. Remember this! Or it will bite you in the butt. 
-		//Use string unless it's in a loop that runs more than 30 times, otherwise use a StringBuilder. 
+		//Use string unless it's in a loop that runs more than 30 times, otherwise use a StringBuilder on the outside of the loop. 
 		//If you pass a string to a method, it's going to be the same as it was before, no matter what you do to it.
 		//Use == for primitives and wrappers but .equals method for ... 
-		// -> 'lambda operator' 
+		// -> is the 'lambda operator' 
 		//Reformat is ctrl shift F if code is going over space. 
 		//.chars method returns an int stream but want to return a regular stream
 		//.mapToObj converting int stream to String stream ==> need for Springboot 
