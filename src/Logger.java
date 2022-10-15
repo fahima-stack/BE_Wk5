b1136e6a3d17f6519e507293f17b6256f547954a
Fahima Begum <haffahima@gmail.com>
2022-10-15 16:52:47 +0100

    Changed name to logger

--- src/Logger.java
+++ src/Logger.java
@@ -1,11 +1,11 @@
 
-public interface exampleInterface { //class can implement multiple interfaces unlike inheritance where you can only inherit one class 
+public interface Logger { //class can implement multiple interfaces unlike inheritance where you can only inherit one class 
 
 /*
  * Difference between abstract class and interface:
  * -Interface doesn't define any method implementations
  * -Abstract class has some abstract methods and some methods that have bodies with implementations defined.
  * -Interface you only define the method signature. Use when you know what the class implementing the interface will do but don't know how you want it to happen. You leave the implementation 100% up to that class.  
- * -Interface is like a contract; when a class implements an interface it has to create a concrete implementation of each of the methods defined in the interface. 
+ * -Interface is a contract; when a class implements an interface it has to create a concrete implementation of each of the methods defined in the interface. 
  */
 }
